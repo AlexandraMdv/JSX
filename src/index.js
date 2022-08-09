6cbb1fe0b39dfc677b615d24fb98f098ffbbc439
AlexandraMdv <109295061+AlexandraMdv@users.noreply.github.com>
2022-08-09 15:32:11 +0300

    image add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellcheck="false">
       My Favourite Foods
     </h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <div>
+      <img src={img + "?grayscale"} alt="randomImage" />
+    </div>
   </div>,
   document.getElementById("root")
 );
